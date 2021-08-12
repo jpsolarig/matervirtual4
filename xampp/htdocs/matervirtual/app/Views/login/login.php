324ae4bc2c122ffa0318c4e21024fff0c825eacd
Jean Pierre <Jean Pierre@DESKTOP-KDUPF3J>
2021-08-12 15:15:28 -0500

    Formulario Login

--- xampp/htdocs/matervirtual/app/Views/login/login.php
+++ xampp/htdocs/matervirtual/app/Views/login/login.php
@@ -11,34 +11,62 @@
         	<div class="col-lg-5">
         		<div class="card shadow-lg border-0 rounded-lg mt-5">
         
-							<div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
-                                    
+							<div class="card-header">
+								<h3 class="text-center font-weight-light my-4">
+									<img id="profile-img" class="logo-imagen" src="<?php echo site_url('imagenes/logo.png'); ?>" />
+								</h3>
+							</div>	
+								
 							<div class="card-body">
                 
-								<form>
-                  <div class="form-floating mb-3">
-                    <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                    <label for="inputEmail">Email address</label>
-                  </div>
-                  <div class="form-floating mb-3">
-                    <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
-                    <label for="inputPassword">Password</label>
-                  </div>
-                  <div class="form-check mb-3">
-                    <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                    <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-                  </div>
-                  <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                    <a class="small" href="password.html">Forgot Password?</a>
-                    <a class="btn btn-primary" href="index.html">Login</a>
-                  </div>
-                </form>
-              
+								<?php 
+									$attributos = ['class' => 'form-login', 'id' => 'form', 'autocomplete' => 'off' ];
+									echo form_open('Login/validarLogin', $attributos);
+								?>	
+									
+								<div class="form-floating mb-3">
+									<?php 
+										$attributosNombre = ['name' =>'input-correo', 'class' => 'form-control',  'type' => 'text', 'placeholder' => 'Correo Electrónico', 'value' => set_value('input-correo')];
+										echo form_input($attributosNombre); 
+									?>	
+									<label id="label-correo">Correo Electrónico</label>
+								</div>
+                  
+								<div class="form-floating mb-3">
+									<?php 
+										$attributosNombre = ['name' =>'input-clave', 'class' => 'form-control',  'type' => 'password', 'placeholder' => 'Contraseña'];
+										echo form_input($attributosNombre); 
+									?>		
+									<label for="inputPassword" >Contraseña</label>
+								</div>	
+									
+								<div class="mt-4 mb-0">
+                <div class="d-grid">
+									<?php 
+										$attributosEnvio = ['class' => 'btn btn-primary btn-block',  'type' => 'submit', 'value' => 'Login' ];
+										echo form_submit($attributosEnvio); 
+									?>	
+								</div>
+              </div>			
+									<!--
+								<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                  <a class="small" href="password.html">Forgot Password?</a>
+                  <a class="btn btn-primary" href="index.html">Login</a>
+                </div>
+                -->
+									
+							
+									
+									
+								<?php echo form_close(); ?> 				
+								
 							</div>
               
+							<!--	
 							<div class="card-footer text-center py-3">
                 <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
               </div>
+							-->
               
 						</div>
           </div>
@@ -47,6 +75,7 @@
     </main>
   </div>
 		
+	<!--	
   <div id="layoutAuthentication_footer">
     <footer class="py-4 bg-light mt-auto">
       <div class="container-fluid px-4">
@@ -61,7 +90,9 @@
       </div>
     </footer>
   </div>
-
+-->
+ 
+ 
 </div>
 				
 <?= $this->endSection() ?>       
